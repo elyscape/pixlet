d45023db0029baa49be2a06bf02856099bc62833
Eli Young <elyscape@gmail.com>
2023-01-23 17:31:17 -0800

    nosanitize test

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -27,10 +27,10 @@ do
 
 	if [[ $ARCH == "linux-arm64"  ]]; then
 		echo "linux-arm64"
-		CC="zig cc -target aarch64-linux-gnu.${GLIBC_VERSION} -isystem /usr/include -L/usr/lib/aarch64-linux-gnu" CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-s -w -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet tidbyt.dev/pixlet
+		CC="zig cc -target aarch64-linux-gnu.${GLIBC_VERSION} -isystem /usr/include -L/usr/lib/aarch64-linux-gnu -fnosanitize=all" CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-s -w -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet tidbyt.dev/pixlet
 	elif [[ $ARCH == "linux-amd64"  ]]; then
 		echo "linux-amd64"
-		CC="zig cc -target x86_64-linux-gnu.${GLIBC_VERSION} -isystem /usr/include -L/usr/lib/x86_64-linux-gnu" CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-s -w -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet tidbyt.dev/pixlet
+		CC="zig cc -target x86_64-linux-gnu.${GLIBC_VERSION} -isystem /usr/include -L/usr/lib/x86_64-linux-gnu -fnosanitize=all" CGO_LDFLAGS="-Wl,-Bstatic -lwebp -lwebpdemux -lwebpmux -Wl,-Bdynamic" CGO_ENABLED=1 GOOS=$RELEASE_PLATFORM GOARCH=$RELEASE_ARCH go build -ldflags="-s -w -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet tidbyt.dev/pixlet
 	elif [[ $ARCH == "windows-amd64"  ]]; then
 		echo "windows-amd64"
 		go build -ldflags="-s -extldflags=-static -X 'tidbyt.dev/pixlet/cmd.Version=${PIXLET_VERSION}'" -o build/${RELEASE_PLATFORM}_${RELEASE_ARCH}/pixlet.exe tidbyt.dev/pixlet
